740a97fd65b0dd143ac7b4794b0a8d4ce127b566
m4c1elz <milaelipe.maciel@gmail.com>
2025-01-31 20:54:49 -0300

    fix(date): fix date timezone issues

--- src/components/post/datetime.tsx
+++ src/components/post/datetime.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import { formatRelative } from 'date-fns'
+import { TZDate } from '@date-fns/tz'
 import { ptBR } from 'date-fns/locale/pt-BR'
 
 interface DateTimeProps {
@@ -7,9 +10,14 @@ interface DateTimeProps {
 }
 
 export function DateTime({ createdAt, updatedAt }: DateTimeProps) {
+    const userTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone
+
+    const zonedDate = new TZDate(createdAt, userTimezone)
+    const zonedTodayDate = new TZDate(new Date(), userTimezone)
+
     return (
         <p className="text-foreground/50 hidden sm:block">
-            {formatRelative(createdAt, new Date(), {
+            {formatRelative(zonedDate, zonedTodayDate, {
                 locale: ptBR,
             })}{' '}
             {updatedAt.getTime() !== createdAt.getTime() && '(editado)'}
