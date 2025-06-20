c33764e8990ab6f8fe9c0700e29ed16371603ad6
m4c1elz <milaelipe.maciel@gmail.com>
2025-06-20 10:14:49 -0300

    refactor: remove useMediaQuery and make navbar render via css

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,13 +1,17 @@
 'use client'
 
-import { useMediaQuery } from '@/hooks/use-media-query'
 import { MobileNavbar } from './mobile'
 import { DesktopNavbar } from './desktop'
 
 export function Navbar() {
-    const isMobile = useMediaQuery('(max-width: 768px)')
-
-    if (isMobile) return <MobileNavbar />
-
-    return <DesktopNavbar />
+    return (
+        <>
+            <div className="block md:hidden">
+                <MobileNavbar />
+            </div>
+            <div className="hidden md:block">
+                <DesktopNavbar />
+            </div>
+        </>
+    )
 }
