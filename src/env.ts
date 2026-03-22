92b762214c2e314f5296ccc90615814fd978682b
m4c1elz <milaelipe.maciel@gmail.com>
2026-03-22 00:41:43 -0300

    chore: remove email password from env vars

--- src/env.ts
+++ src/env.ts
@@ -4,7 +4,6 @@ const envSchema = z.object({
     DATABASE_URL: z.string().url(),
     AUTH_SECRET: z.string(),
     EMAIL_SENDER_ADDRESS: z.string().email(),
-    EMAIL_SENDER_PASSWORD: z.string(),
     EMAIL_JWT_SECRET: z.string(),
     EMAIL_REDIRECT_URL: z.string().url(),
     NODE_ENV: z.enum(['production', 'development', 'test']),
