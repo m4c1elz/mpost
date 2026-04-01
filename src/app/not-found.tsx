f50c6b5255325ee7c533509eeef7d69712631704
m4c1elz <milaelipe.maciel@gmail.com>
2026-03-31 22:41:00 -0300

    feat: add translations to the global 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,20 +1,21 @@
 import Link from 'next/link'
 import { MapPinX } from 'lucide-react'
 import { Button } from '@/components/ui/button'
+import { getTranslations } from 'next-intl/server'
 
-export default function NotFound() {
+export default async function NotFound() {
+    const t = await getTranslations('common.notFound')
+
     return (
         <main className="text-center h-96 grid place-content-center space-y-4">
             <h1 className="text-2xl font-bold mb-4">MPost</h1>
             <MapPinX className="text-destructive mx-auto mb-2" size={50} />
             <div>
-                <h1 className="text-xl font-bold">Que esquisito.</h1>
-                <p className="font-medium">
-                    Parece que essa página não existe.
-                </p>
+                <h1 className="text-xl font-bold">{t('title')}</h1>
+                <p className="font-medium">{t('description')}</p>
             </div>
             <Button variant="link" asChild>
-                <Link href="/">Voltar para a página principal</Link>
+                <Link href="/">{t('backToHomeLink')}</Link>
             </Button>
         </main>
     )
