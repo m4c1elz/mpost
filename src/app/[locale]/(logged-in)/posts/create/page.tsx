1ce7f1bdaa8f69d4207b45631182f20315d6b194
m4c1elz <milaelipe.maciel@gmail.com>
2026-03-31 21:26:41 -0300

    feat: add translations for create post page and actions

--- src/app/[locale]/(logged-in)/posts/create/page.tsx
+++ src/app/[locale]/(logged-in)/posts/create/page.tsx
@@ -6,13 +6,16 @@ import {
     CardContent,
 } from '@/components/ui/card'
 import { CreatePostForm } from '@/features/posts/components/create-post-form'
+import { getTranslations } from 'next-intl/server'
 
-export default function CreatePost() {
+export default async function CreatePost() {
+    const t = await getTranslations('posts.create')
+
     return (
         <Card className="w-full md:w-[450px] m-auto">
             <CardHeader>
-                <CardTitle>Criar postagem</CardTitle>
-                <CardDescription>Fale um pouco do seu dia!</CardDescription>
+                <CardTitle>{t('title')}</CardTitle>
+                <CardDescription>{t('description')}</CardDescription>
             </CardHeader>
             <CardContent className="space-y-4">
                 <CreatePostForm />
--- src/features/posts/actions/create-post.ts
+++ src/features/posts/actions/create-post.ts
@@ -4,19 +4,20 @@ import { auth } from '@/auth'
 import { redirect } from 'next/navigation'
 import { z } from 'zod'
 import { createPost as createPostFromDb } from '../services/create-post'
+import { _Translator } from 'next-intl'
+import { getTranslations } from 'next-intl/server'
 
-const createPostSchema = z
-    .string()
-    .max(140, 'Postagem não deve conter mais de 140 caracteres.')
-    .nonempty('Postagem não pode estar vazia.')
-    .trim()
+const createPostSchema = (t: _Translator) =>
+    z.string().max(140, t('maxChars')).nonempty(t('nonEmpty')).trim()
 
 export async function createPost(_prevState: unknown, formData: FormData) {
     const session = await auth()
     const user = session!.user!
     const post = formData.get('post')
 
-    const { success, data, error } = createPostSchema.safeParse(post)
+    const t = await getTranslations('posts.create.validation')
+
+    const { success, data, error } = createPostSchema(t).safeParse(post)
 
     if (!success) {
         return {
